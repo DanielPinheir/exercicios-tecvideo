0b90cdec6fa047e230af3298de893f580fd267e9
Daniel Pinheiro Machado <dyelzim@gmail.com>
2022-12-05 20:11:55 -0300

    create a exercise one about math operations

---
+++ 1-operacoesMatemáticas/script.js
@@ -0,0 +1,9 @@
+//create a math operations function
+const mathOperations = (number1, number2) => {
+  console.log(`Sum: ${number1 + number2}`);
+  console.log(`Subtraction: ${number1 - number2}`);
+  console.log(`Division: ${number1 / number2}`);
+  console.log(`Multiplication: ${number1 * number2}`);
+};
+
+mathOperations(2, 4);
